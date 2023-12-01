b08287d4b47308afaf93d56c6034d59a0a7ee668
Valentyna Koshelnyk <vakoshelnik@gmail.com>
2023-12-01 09:37:59 +0100

    add dbConnection

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,18 @@
 package org.example;
 
+import org.glassfish.jersey.grizzly2.httpserver.GrizzlyHttpServerFactory;
+import org.glassfish.jersey.server.ResourceConfig;
+
+import java.net.URI;
+
 public class Main {
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String BASE_URI = "http://localhost:8080/";
+        ResourceConfig resourceConfig = new ResourceConfig(class, .class);
+        GrizzlyHttpServerFactory.createHttpServer(URI.create(BASE_URI), resourceConfig);
+        System.out.println("Server started at: " + BASE_URI);
+
+
     }
 }
